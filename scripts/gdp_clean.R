5a57072d8e170e25b8f46cc2b807cee4525822ac
sushanthvk02 <viswasushanth.k18@gmail.com>
2025-04-08 13:19:59 -0700

    added 2 raw datasets and modifed the cleaned gdp datset and script

--- scripts/gdp_clean.R
+++ scripts/gdp_clean.R
@@ -1,30 +1,32 @@
 # Load libraries
 library(tidyverse)
-library(zoo)
 
-# Read the raw data, skip unnecessary rows, recognize all forms of empty as NA
+# Read the raw data, remove unnecessary rows, clean up NAs
 gdp_raw <- read.csv("data/raw/GDP_Growth - WBG.csv", skip = 4, na.strings = c("", "NA", "..", "n/a"))
-head(gdp_raw)
 
-# Keep only Country and year columns
+# Keep only Country and years columns
 gdp_clean <- gdp_raw[, c(1, 5:ncol(gdp_raw) - 1)]
 
-# Rename Country column
+# Rename first column
 colnames(gdp_clean)[1] <- "Country"
 
-# Clean year column names: X1990 → 1990
+# Clean year column names (remove "X")
 colnames(gdp_clean)[2:ncol(gdp_clean)] <- gsub("^X", "", colnames(gdp_clean)[2:ncol(gdp_clean)])
-head(gdp_clean)
 
-# Keep only years 1990 to 2023
+# Keep only years 1990–2023
 years_to_keep <- as.character(1990:2023)
 gdp_trimmed <- gdp_clean[, c("Country", years_to_keep)]
 
-# Convert all year columns to numeric
-gdp_trimmed[ , 2:ncol(gdp_trimmed)] <- lapply(gdp_trimmed[ , 2:ncol(gdp_trimmed)], function(x) as.numeric(as.character(x)))
-
+# Converting to Long format
+gdp_long <- gdp_trimmed %>%
+  pivot_longer(-Country, names_to = "Year", values_to = "GDP_Growth_Rate") %>%
+  mutate(
+    Year = as.numeric(Year),
+    GDP_Growth_Rate = as.numeric(GDP_Growth_Rate)
+  )
 
 # Save to cleaned folder
-write.csv(gdp_trimmed, "data/cleaned/GDP_Growth_rate.csv", row.names = FALSE)
+write.csv(gdp_long, "data/cleaned/GDP_Growth_Rate.csv", row.names = FALSE)
+
 
 
